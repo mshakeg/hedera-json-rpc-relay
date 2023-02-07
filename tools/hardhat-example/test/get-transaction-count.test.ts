f70a0d114eb17c96fc4367973f7434c4b86d1b54
Mohammed Shaikjee <shaikjeemohammed@gmail.com>
2023-02-07 08:33:23 +0200

    ++ get-transaction-count

---
+++ tools/hardhat-example/test/get-transaction-count.test.ts
@@ -0,0 +1,57 @@
+import { ethers } from 'hardhat';
+import {
+  PromiseStatus
+} from './utils'
+
+describe('Get block transactions in parallel', function () {
+  const startBlock = 522_900;
+  const endBlock = 523_900;
+  const testCases = [10]; // block ranges , 100, 1_000
+
+  async function getParallelBlockTransactionsCount(startBlock: number, endBlock: number, blockRange: number) {
+
+    let blockNumber;
+
+    for (let i = startBlock; i < endBlock; i += blockRange) {
+
+      const promises: Array<Promise<number>> = [];
+      const upper = Math.min(i + blockRange, endBlock);
+
+      console.log(`getting block txs count range: [${i}, ${upper}]`);
+
+      for (let j = i; j < upper; j++) {
+
+        blockNumber = ethers.BigNumber.from(j);
+
+        promises.push(
+          ethers.provider.send('eth_getBlockTransactionCountByNumber', [blockNumber._hex, false])
+        );
+      }
+
+      // wait for all promises to settle before proceeding onto getting next block range
+      const results = await Promise.allSettled(promises);
+
+      for (const blockResult of results) {
+
+        if (blockResult.status === PromiseStatus.FULFILLED) {
+
+          const blockTxCount = blockResult.value;
+
+          console.log('Block success:', blockTxCount);
+
+        } else {
+
+          const error = blockResult.reason;
+          console.log('Block failed message:', error.message);
+        }
+      }
+
+    }
+  }
+
+  for (const testCase of testCases) {
+    it(`gets ${testCase} blocks in parallel`, async () => {
+      await getParallelBlockTransactionsCount(startBlock, endBlock, testCase);
+    });
+  }
+});
