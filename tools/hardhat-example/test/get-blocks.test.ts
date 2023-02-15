18087ed8b58ca8a2ea7e1ea0d9ad8a440ca2cdfb
MShakeG <40227090+mshakeg@users.noreply.github.com>
2023-02-15 19:26:31 +0200

    Get many blocks in parallel (#16)
    * ++ @ethersproject/providers & get parallel blocks
    
    * ++ endBlock to get-blocks.test
    
    * wait for current block range promises to settle
    
    * log getting block range
    
    * +- endBlock
    
    * ++ get-transaction-count
    
    * ++ proper hex formatting of blockNumber
    
    * log decimal values for blockTxCount

---
+++ tools/hardhat-example/test/get-blocks.test.ts
@@ -0,0 +1,55 @@
+import { expect } from 'chai';
+import { Block } from '@ethersproject/providers';
+import { ethers } from 'hardhat';
+import {
+  PromiseStatus
+} from './utils'
+
+describe('Get blocks in parallel', function () {
+  const startBlock = 522_900;
+  const endBlock = 523_900;
+  const testCases = [10, 100, 1_000]; // block ranges
+
+  async function getParallelBlocks(startBlock: number, endBlock: number, blockRange: number) {
+
+    for (let i = startBlock; i < endBlock; i += blockRange) {
+
+      const promises = [];
+      const upper = Math.min(i + blockRange, endBlock);
+
+      console.log(`getting block range: [${i}, ${upper}]`);
+
+      for (let j = i; j < upper; j++) {
+        promises.push(
+          ethers.provider.getBlock(j)
+        );
+      }
+
+      // wait for all promises to settle before proceeding onto getting next block range
+      const results = await Promise.allSettled(promises);
+
+      for (const blockResult of results) {
+
+        if (blockResult.status === PromiseStatus.FULFILLED) {
+
+          const value = blockResult.value as Block;
+          const blockNumber = value.number;
+
+          console.log('Block success:', blockNumber);
+
+        } else {
+
+          const error = blockResult.reason;
+          console.log('Block failed message:', error.message);
+        }
+      }
+
+    }
+  }
+
+  for (const testCase of testCases) {
+    it(`gets ${testCase} blocks in parallel`, async () => {
+      await getParallelBlocks(startBlock, endBlock, testCase);
+    });
+  }
+});
--- tools/hardhat-example/test/utils/index.ts
+++ tools/hardhat-example/test/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './address';
 export * from './constants';
 export * from './encoding';
+export * from './formatting';
 export * from './hedera';
 export * from './numbers';
 export * from './utils';
