88c8423e8c0299c883e9234a71725b5458515c6b
Mohammed Shaikjee <shaikjeemohammed@gmail.com>
2023-03-12 21:33:14 +0200

    formatted DeletageCall.sol

--- tools/hardhat-example/contracts/DelegateCall.sol
+++ tools/hardhat-example/contracts/DelegateCall.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.0;
 import 'hardhat/console.sol';
 
 interface ICaller {
-	function callback() external;
+    function callback() external;
 }
 
 // routes calls via a Router
 contract SuperRouter is ICaller {
-
     address public immutable routerAddress;
     address public immutable precompileAddress;
 
@@ -18,27 +17,25 @@ contract SuperRouter is ICaller {
         precompileAddress = _precompileAddress;
     }
 
-	function routeViaRouter() external {
+    function routeViaRouter() external {
+        console.log('in SuperRouter#routeViaRouter; origin is %s', tx.origin);
 
-		console.log("in SuperRouter#routeViaRouter; origin is %s", tx.origin);
+        Router(routerAddress).route();
+    }
 
-		Router(routerAddress).route();
-	}
+    function callback() external override {
+        console.log('in SuperRouter#callback; origin is %s', tx.origin);
+        console.log('in SuperRouter#callback; sender is %s', msg.sender);
 
-	function callback() external override {
-		console.log("in SuperRouter#callback; origin is %s", tx.origin);
-		console.log("in SuperRouter#callback; sender is %s", msg.sender);
-
-		(bool success, ) = precompileAddress.delegatecall(abi.encodeWithSignature('fee()'));
-	}
-
+        (bool success, ) = precompileAddress.delegatecall(abi.encodeWithSignature('fee()'));
+    }
 }
 
 contract Router is ICaller {
     address public immutable coreAddress;
     address public immutable precompileAddress;
 
-	bool internal isNormalRoute;
+    bool internal isNormalRoute;
 
     constructor(address _coreAddress, address _precompileAddress) {
         coreAddress = _coreAddress;
@@ -46,43 +43,41 @@ contract Router is ICaller {
     }
 
     function route() external {
+        isNormalRoute = false;
 
-		isNormalRoute = false;
-
-		console.log("in Router#route; Core address is %s", coreAddress);
-		console.log("in Router#route; this address is %s", address(this));
+        console.log('in Router#route; Core address is %s', coreAddress);
+        console.log('in Router#route; this address is %s', address(this));
 
         (bool success, ) = coreAddress.delegatecall(abi.encodeWithSignature('core()'));
         require(success, 'Delegatecall to Core failed');
     }
 
-	function normalRoute() external {
+    function normalRoute() external {
+        isNormalRoute = true;
 
-		isNormalRoute = true;
+        Core(coreAddress).core();
+    }
 
-		Core(coreAddress).core();
-	}
+    function callback() external override {
+        console.log('in Router#callback; origin is %s', tx.origin);
+        console.log('in Router#callback; sender is %s', msg.sender);
 
-	function callback() external override {
-		console.log("in Router#callback; origin is %s", tx.origin);
-		console.log("in Router#callback; sender is %s", msg.sender);
-
-		(bool success, ) = precompileAddress.delegatecall(abi.encodeWithSignature('precompile()'));
-	}
+        (bool success, ) = precompileAddress.delegatecall(abi.encodeWithSignature('precompile()'));
+    }
 }
 
 contract Precompile {
-	function precompile() external {
-		console.log("in Precompile#precompile; origin is %s", tx.origin);
-		console.log("in Precompile#precompile; sender is %s", msg.sender);
-	}
+    function precompile() external {
+        console.log('in Precompile#precompile; origin is %s', tx.origin);
+        console.log('in Precompile#precompile; sender is %s', msg.sender);
+    }
 }
 
 contract Core {
     function core() external {
-
-		console.log("in Core#core; origin is %s", tx.origin);
-		console.log("in Core#core; sender is %s", msg.sender);
+		console.log('in Core#core; this is %s', address(this)); /// @dev this changes not !Core.address if delegatecall??
+        console.log('in Core#core; origin is %s', tx.origin);
+        console.log('in Core#core; sender is %s', msg.sender);
 
         ICaller(msg.sender).callback();
     }
