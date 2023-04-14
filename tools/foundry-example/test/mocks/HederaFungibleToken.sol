47170ffaf88d94862749f729238aa2ac67aaa253
Mo Shaikjee <shaikjeemohammed@gmail.com>
2023-04-14 10:14:39 +0200

    ++ internal auth funcs to HederaFungibleToken

--- tools/foundry-example/test/mocks/HederaFungibleToken.sol
+++ tools/foundry-example/test/mocks/HederaFungibleToken.sol
@@ -5,9 +5,12 @@ import 'openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
 
 import 'hedera-smart-contracts/hts-precompile/HederaResponseCodes.sol';
 import 'hedera-smart-contracts/hts-precompile/IHederaTokenService.sol';
+import 'hedera-smart-contracts/hts-precompile/KeyHelper.sol';
 
-contract HederaFungibleToken is ERC20 {
+contract HederaFungibleToken is ERC20, KeyHelper {
     IHederaTokenService.FungibleTokenInfo public fungibleTokenInfo;
+    // key -> value e.g. 1 -> 0x123 means that the ADMIN is account 0x123
+    mapping(uint => address) internal tokenKeys;
 
     constructor(
         IHederaTokenService.FungibleTokenInfo memory _fungibleTokenInfo
@@ -31,7 +34,12 @@ contract HederaFungibleToken is ERC20 {
         // Copy the tokenKeys array
         uint256 length = _fungibleTokenInfo.tokenInfo.token.tokenKeys.length;
         for (uint256 i = 0; i < length; i++) {
-            fungibleTokenInfo.tokenInfo.token.tokenKeys.push(_fungibleTokenInfo.tokenInfo.token.tokenKeys[i]);
+            IHederaTokenService.TokenKey memory tokenKey = _fungibleTokenInfo.tokenInfo.token.tokenKeys[i];
+            fungibleTokenInfo.tokenInfo.token.tokenKeys.push(tokenKey);
+
+            /// @dev contractId can in fact be any address including an EOA address
+            ///      The KeyHelper lists 5 types for KeyValueType; however only CONTRACT_ID is considered
+            tokenKeys[tokenKey.keyType] = tokenKey.key.contractId;
         }
 
         fungibleTokenInfo.tokenInfo.token.expiry.second = _fungibleTokenInfo.tokenInfo.token.expiry.second;
@@ -55,5 +63,63 @@ contract HederaFungibleToken is ERC20 {
 
         fungibleTokenInfo.tokenInfo.ledgerId = _fungibleTokenInfo.tokenInfo.ledgerId;
         fungibleTokenInfo.decimals = _fungibleTokenInfo.decimals;
+    }
+
+    function _isAccountOriginOrSender(address account) internal view returns (bool) {
+        return _isAccountOrigin(account) || _isAccountSender(msg.sender);
+    }
+
+    function _isAccountOrigin(address account) internal view returns (bool) {
+        return account == tx.origin;
+    }
+
+    function _isAccountSender(address account) internal view returns (bool) {
+        return account == msg.sender;
+    }
+
+    function _hasTreasurySig() internal view returns (bool) {
+        return _isAccountOriginOrSender(fungibleTokenInfo.tokenInfo.token.treasury);
+    }
+
+    function _hasAdminKeySig() internal view returns (bool) {
+        return _isAccountOriginOrSender(getKey(KeyHelper.KeyType.ADMIN));
+    }
+
+    function _hasKycKeySig() internal view returns (bool) {
+        return _isAccountOriginOrSender(getKey(KeyHelper.KeyType.KYC));
+    }
+
+    function _hasFreezeKeySig() internal view returns (bool) {
+        return _isAccountOriginOrSender(getKey(KeyHelper.KeyType.FREEZE));
+    }
+
+    function _hasWipeKeySig() internal view returns (bool) {
+        return _isAccountOriginOrSender(getKey(KeyHelper.KeyType.WIPE));
+    }
+
+    function _hasSupplyKeySig() internal view returns (bool) {
+        return _isAccountOriginOrSender(getKey(KeyHelper.KeyType.SUPPLY));
+    }
+
+    function _hasFeeScheduleKeySig() internal view returns (bool) {
+        return _isAccountOriginOrSender(getKey(KeyHelper.KeyType.FEE));
+    }
+
+    function _hasPauseKeySig() internal view returns (bool) {
+        return _isAccountOriginOrSender(getKey(KeyHelper.KeyType.PAUSE));
+    }
+
+    // public/external state-changing functions
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        return super.transferFrom(sender, recipient, amount);
+    }
+
+    // public/external view functions
+    function getKey(KeyHelper.KeyType keyType) public view returns (address keyOwner) {
+        keyOwner = tokenKeys[keyTypes[keyType]];
     }
 }
